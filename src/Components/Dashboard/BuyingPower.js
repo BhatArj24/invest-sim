5754eca584240d7c787cda4ab02763d4c41a51e3
BhatArj24 <56486136+BhatArj24@users.noreply.github.com>
2024-03-16 16:48:29 -0700

    buying power

--- src/Components/Dashboard/BuyingPower.js
+++ src/Components/Dashboard/BuyingPower.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import {collection, getDocs, updateDoc, doc, arrayUnion} from "firebase/firestore";
 import {db} from "../../firebase-config.js";
 import NavBar from '../NavBar.js';
+import {useNavigate } from 'react-router-dom';
 
 
 const BuyingPower = () => {
@@ -9,7 +10,7 @@ const BuyingPower = () => {
     const [user, setUser] = useState({email:"",username:"",myStocks:[],balance:0,operations:[],invested:0});
     const userCollectionRef = collection(db,"users");
     const [amount, setAmount] = useState();
-
+    const navigate = useNavigate();
 
     const getProfile = async () =>{
         const email = sessionStorage.getItem("email");
@@ -28,9 +29,11 @@ const BuyingPower = () => {
         });
     }  
 
-    const handleIncrease = () =>{
-
-        console.log("amount: ", amount);
+    const handleIncrease = async () =>{
+        await updateDoc(doc(db, "users", user.email), {
+            balance: user.balance + amount
+        });
+        navigate("/dashboard");
     }
     
 
@@ -49,8 +52,8 @@ const BuyingPower = () => {
             <div className="bg-white border-2 border-black w-1/3 m-auto rounded-lg" style={{height:"325px"}}>
                     <div className="flex">
                         <div>
-                            <h1 className="text-4xl font-bold px-10 pt-8">Operation</h1>
-                            <h1 className="text-2xl font-medium px-10 pt-1">Buy</h1>
+                            <h1 className="text-4xl font-bold px-10 pt-8">Buying Power</h1>
+                            <h1 className="text-2xl font-medium px-10 pt-1">Increase</h1>
                         </div>
                         <div>
                             <h1 className="text-xl font-bold px-60 pt-8"></h1>
@@ -59,9 +62,6 @@ const BuyingPower = () => {
                     </div>
 
                     <div className="pt-5 mx-10">
-                        <div className="flex">
-                            <h1 className="text-xl">Price: $</h1>
-                        </div>
                         <div className="flex pt-3">
                             <h1 className="text-xl">Amount:</h1>
                             <input 
@@ -82,7 +82,7 @@ const BuyingPower = () => {
 
                     <div className="flex pt-10 px-10">
                         <h1 className="text-xl">Total Buying Power: ${(user.balance + amount).toFixed(2)}</h1>
-                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-10 rounded" style={{marginLeft:"50%"}} onClick={handleIncrease}>Buy</button>
+                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-10 rounded" style={{marginLeft:"50%"}} onClick={handleIncrease}>Confirm</button>
 
                     </div>
                 </div>
